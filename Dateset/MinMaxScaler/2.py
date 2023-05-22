740f364b2e85a4ff8f396baf68f6b3a405374fd5
menna161 <57011308+menna161@users.noreply.github.com>
2023-05-23 00:42:43 +0300

    Delete Dateset/MinMaxScaler directory

--- Dateset/MinMaxScaler/2.py
+++
@@ -1,5 +0,0 @@
-from sklearn.preprocessing import MinMaxScaler
-
-scaler = MinMaxScaler()
-scaler.fit(data)    
-print(scaler.transform([[2, 2]]))
--- Dateset/MinMaxScaler/4.py
+++
@@ -1,5 +0,0 @@
-from sklearn.preprocessing import MinMaxScaler
-
-X_scaler = MinMaxScaler().fit(X_train)
-X_train_scaled = X_scaler.transform(X_train)
-X_test_scaled = X_scaler.transform(X_test)
